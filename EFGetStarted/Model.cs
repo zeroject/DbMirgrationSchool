b51ec6117afdf0a94773705357dd7af1f827268a
JensIssa <71692396+JensIssa@users.noreply.github.com>
2024-03-06 17:47:45 +0100

    Added the migration for the Category,, divided the differents models in different files, and updated the guidance to do the migrations.

--- EFGetStarted/Model.cs
+++ EFGetStarted/Model.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFGetStarted.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 
 public class MigrationContext : DbContext
 {
-    public DbSet<Products> Products { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
 
     public MigrationContext()
     {
@@ -14,20 +17,16 @@ public class MigrationContext : DbContext
     // The following configures EF to create a Sqlite database file in the
     // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlServer("Server=localhost;Database=migrationDb;User Id=sa;Password=Hyy89xjw!;Trusted_Connection=False;TrustServerCertificate=True;");
+        => options.UseSqlServer("Server=localhost;Database=migrationDb;User Id=sa;Password=SuperSecret7!;Trusted_Connection=False;TrustServerCertificate=True;");
 }
+
 
 //add the relevant new table here with its properties and ensure that a DbSet is created for it in the MigrationContext class.
 //for futher help. dont ask Kasper. he is tired of your questions.
-public class Products
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public float Price { get; set; }
-}
 
 //after adding the new table, run the following commands in the terminal
 //dotnet ef migrations add [migrationName]
 //dotnet ef database update
+//If the CLI is not installed, you can install it by running the following command: dotnet tool install --global dotnet-ef
 
 
